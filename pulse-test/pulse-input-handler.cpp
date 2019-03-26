7f85511f12837327a53aa6691cd19692289c931d
dylan.ka <dylan.ka@kakaocorp.com>
2019-03-26 19:10:05 +0900

    pulse simple api test

---
+++ pulse-test/pulse-input-handler.cpp
@@ -0,0 +1,181 @@
+/****************************************************
+ * Simple API Instance using pulse simple API
+ * This will record a mic input from pulse audio 
+ * ffkaka
+ ***************************************************/
+
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <locale.h>
+
+// For get the pid in user space
+#include <sys/types.h>
+#include <sys/syscall.h>
+
+#include <pulse/error.h>
+#include <pulse-input-handler.h>		
+#include "RingBuff.hpp"
+
+#define INPUT_UNIT_SIZE 768
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+RingBuff gBuff;
+
+int getFormatSize(pa_sample_format_t format)
+{
+	int ret = 1;
+	switch (format)
+	{
+		case PA_SAMPLE_U8:
+		case PA_SAMPLE_ALAW:
+		case PA_SAMPLE_ULAW:
+			ret = 1;
+			break;
+		case PA_SAMPLE_S16LE:
+		case PA_SAMPLE_S16BE:
+			ret = 2;
+			break;
+		case PA_SAMPLE_S24LE:
+		case PA_SAMPLE_S24BE:
+			ret = 3;
+			break;
+		case PA_SAMPLE_FLOAT32LE:
+		case PA_SAMPLE_FLOAT32BE:
+		case PA_SAMPLE_S32LE:
+		case PA_SAMPLE_S32BE:
+		case PA_SAMPLE_S24_32LE:
+		case PA_SAMPLE_S24_32BE:
+			ret = 4;
+			break;
+		default :
+			break;
+	}
+	return ret;
+}
+
+PulseInputHandler::PulseInputHandler(int rate, in_read_format_t format, int channels) {
+	sample_spec = pa_xnew0(pa_sample_spec, 1);
+	sample_spec->format = PA_SAMPLE_S24_32LE;
+	sample_spec->rate = 48000;
+	sample_spec->channels = (uint8_t)1;
+	readThread = 0; needRun = false;
+	userRate = rate;
+	userFormat = format;
+}
+
+PulseInputHandler::~PulseInputHandler() {
+	if (stream) {
+		freeStream();
+	}
+	pa_xfree(sample_spec);
+	if (channel_map) pa_xfree(channel_map);
+	if (stream_name) pa_xfree(stream_name);
+	if (client_name) pa_xfree(client_name);
+}
+
+void PulseInputHandler::DIH_CreateStream(char* streamName) {
+	createStream(streamName);
+}
+
+void PulseInputHandler::DIH_FreeStream() {
+	freeStream();
+}
+
+void PulseInputHandler::DIH_readDataSync(void* buf, size_t bytes) {
+	readDataSync(buf, bytes);
+}
+
+void PulseInputHandler::createStream(char *streamName) {
+	stream_name = pa_xstrdup( streamName? streamName : "test-input");
+
+	int error;
+	stream = pa_simple_new(NULL,
+			stream_name,
+			PA_STREAM_RECORD,
+			"regular0",
+			stream_name,
+			sample_spec,
+			channel_map,
+			NULL,
+			&error);
+	if (!stream) {
+		printf("[%u:%lu] pa_simple_new failed: %s\n",
+				getpid(), syscall(__NR_gettid), pa_strerror(error));
+		return;
+	}
+
+	pthread_mutex_lock(&mutex);
+	needRun = true;
+	error = pthread_create(&readThread, NULL, readThreadFunc, this);
+	if (error < 0) {
+		printf("[%u:%lu] fail to create thread: \n", getpid(), syscall(__NR_gettid));
+		pthread_mutex_unlock(&mutex);
+		return;
+	}
+	pthread_cond_wait(&cond, &mutex);
+	pthread_mutex_unlock(&mutex);
+}
+
+void* PulseInputHandler::readThreadFunc(void* data) {
+	pthread_mutex_lock(&mutex);
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&mutex);
+
+	assert(data);
+	auto pthis = static_cast<PulseInputHandler*>(data);
+	int error = 0;
+
+	printf("[%u:%lu] Thread Started\n", getpid(), syscall(__NR_gettid));
+	while (true) {
+		uint8_t buf[INPUT_UNIT_SIZE * getFormatSize(pthis->sample_spec->format) * pthis->sample_spec->channels] = {0,};
+		pthread_mutex_lock(&mutex);
+		if (!pthis->needRun) break;
+		if (pa_simple_read(pthis->stream, buf, sizeof(buf), &error) < 0) {
+			printf("[%u:%lu] failt to read simple data (%s)\n",
+					getpid(), syscall(__NR_gettid), pa_strerror(error));
+		}
+		else {
+			gBuff.putData(buf, sizeof(buf));
+			if (gBuff.getReadableSize() >= sizeof(buf)) {
+				pthread_cond_signal(&cond);
+			}
+		}
+		pthread_mutex_unlock(&mutex);
+		usleep(5000);
+	}
+	return nullptr;
+}
+
+void PulseInputHandler::freeStream() {
+	if (stream) {
+		if (readThread && needRun) {
+			needRun = false;
+			void *ret = nullptr;
+			pthread_join(readThread, &ret);  // This will unref readThread;
+			readThread = 0;
+		}
+		pa_simple_free(stream);
+		stream = nullptr;
+	}
+}
+
+void PulseInputHandler::readDataSync(void *buf, size_t bytes) {
+	static uint32_t totalFeed = 0;
+	pthread_mutex_lock(&mutex);
+	pthread_cond_wait(&cond, &mutex);
+	if ( totalFeed > userRate * getFormatSize((pa_sample_format_t)userFormat) * 5) {
+		printf("[%u:%lu] 5sec data recorded\n", getpid(), syscall(__NR_gettid));
+		totalFeed = 0;
+	}
+	gBuff.getData(buf, bytes);
+	totalFeed += bytes;
+	pthread_mutex_unlock(&mutex);
+}
+
---
+++ pulse-test/pulse-input-handler.h
@@ -0,0 +1,99 @@
+#ifndef __PULSE_INPUT_HANDLER_H__
+#define __PULSE_INPUT_HANDLER_H__
+
+
+#include <map>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <pulse/pulseaudio.h>
+#include <pulse/simple.h>
+#include <pthread.h>
+
+typedef enum in_read_format {
+    SAMPLE_U8,
+    SAMPLE_ALAW,
+    /**< 8 Bit a-Law */
+
+    SAMPLE_ULAW,
+    /**< 8 Bit mu-Law */
+
+    SAMPLE_S16LE,
+    /**< Signed 16 Bit PCM, little endian (PC) */
+
+    SAMPLE_S16BE,
+    /**< Signed 16 Bit PCM, big endian */
+
+    SAMPLE_FLOAT32LE,
+    /**< 32 Bit IEEE floating point, little endian (PC), range -1.0 to 1.0 */
+
+    SAMPLE_FLOAT32BE,
+    /**< 32 Bit IEEE floating point, big endian, range -1.0 to 1.0 */
+
+    SAMPLE_S32LE,
+    /**< Signed 32 Bit PCM, little endian (PC) */
+
+    SAMPLE_S32BE,
+    /**< Signed 32 Bit PCM, big endian */
+
+    SAMPLE_S24LE,
+    /**< Signed 24 Bit PCM packed, little endian (PC). \since 0.9.15 */
+
+    SAMPLE_S24BE,
+    /**< Signed 24 Bit PCM packed, big endian. \since 0.9.15 */
+
+    SAMPLE_S24_32LE,
+    /**< Signed 24 Bit PCM in LSB of 32 Bit words, little endian (PC). \since 0.9.15 */
+
+    SAMPLE_S24_32BE,
+    /**< Signed 24 Bit PCM in LSB of 32 Bit words, big endian. \since 0.9.15 */
+
+    SAMPLE_MAX,
+    /**< Upper limit of valid sample types */
+
+    SAMPLE_INVALID = -1
+    /**< An invalid value */
+} in_read_format_t;
+
+
+/*
+ * This Class will create a pulse simple audio stream and will provide method which can 
+ *  get a data with sync way. 
+ * Supported rate : 16000, 32000, 48000
+ * Supported format : SAMPLE_S32LE, SAMPLE_S16LE
+ * Supported channels : only mono channel is supported now
+ *  !!! pingpong only support the mono/16K/SAMPLE_S16LE
+ */
+
+class PulseInputHandler {
+	public:
+		PulseInputHandler(int rate=48000, in_read_format_t format = SAMPLE_S32LE, int channels = 1);
+		~PulseInputHandler();
+
+		void DIH_CreateStream(char* streamName = nullptr);
+		void DIH_FreeStream();
+		void DIH_readDataSync(void* buf, size_t bytes);
+
+	private:
+		char *stream_name;
+		char *client_name;
+		char *device;
+		size_t latencyByte;
+		pa_simple *stream;
+		pa_sample_spec *sample_spec;
+		pa_channel_map *channel_map;
+		pthread_t readThread;
+		bool needRun;
+		uint32_t readBufSize;
+		uint32_t feedBufSize;
+		int userRate;
+		in_read_format_t userFormat;
+
+		void createStream(char* streamName);
+		void freeStream();
+		void readDataSync(void* buf, size_t bytes);
+		static void* readThreadFunc(void* data);
+};
+
+#endif //__PULSE_INPUT_HANDLER_H__
